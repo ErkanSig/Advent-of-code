27cfffbc57df77f1aca1df261a9122bc8b0da07e
ErkanSig <trainee.erkan.hassan@sigmalabs.co.uk>
2022-12-02 12:48:46 +0000

    Day 1

---
+++ Advent_of_code_1.py
@@ -0,0 +1,18 @@
+import requests
+import pandas as pd
+calories = pd.read_csv('./Data/calories.csv', sep = "\s+|\t+|\s+\t+|\t+\s+")
+# print(calories.head(20))
+calories_list = []
+calories_count = 0
+calories['Calories'] = calories['Calories'].astype(int)
+for item in calories['Calories']:
+    if item > 0:
+        calories_count += item
+    else:
+        calories_list.append(calories_count)
+        calories_count = 0
+
+calories_list_sorted = sorted(calories_list, reverse=True)
+print(calories_list_sorted[:3])
+top3_total = calories_list_sorted[0] + calories_list_sorted[1] + calories_list_sorted[2]
+print(top3_total)
